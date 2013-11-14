7c472e40cc0d5823fe119fe68ba05d1b17ebe615
szwagier90 <szwagier90@gmail.com>
2013-11-14 01:14:36 +0100

    Core ARM code created
    RCC: Configured
    GPIO: set to GPIOA output port and USART1 port
    USART1: set to RX and TX
    NVIC: Set to USART1 RXNE
    USART1_IRQ_HANDLER: tested and working
    SysTick_Handler: tested and working

--- Project/main.c
+++ Project/main.c
@@ -1,8 +1,114 @@
+#include "stm32f0xx_gpio.h"
+#include "stm32f0xx_rcc.h"
+#include "stm32f0xx_usart.h"
+#include "stm32f0xx_misc.h"
+
+void RCC_conf(void);
+void GPIO_conf(void);
+void USART_conf(void);
+void NVIC_conf(void);
+
+volatile uint8_t sign = '0';
+volatile uint8_t w_received = 0;
 
 int main(void)
 {
+    RCC_conf();
+    GPIO_conf();
+    USART_conf();
+    NVIC_conf();
+
+    w_received = 0;
 
     while(1)
     {
     }
+}
+
+void SysTick_Handler(void)
+{
+    if(w_received)
+    {
+        sign = 'W';
+        GPIO_Write(GPIOA, sign);
+    }
+    else
+    {
+        sign += 1;
+        GPIO_Write(GPIOA, sign);
+
+    }
+
+    USART_SendData(USART1, sign);
+}
+
+void USART1_IRQHandler(void)
+{
+    if(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == SET)
+    {
+        sign = USART_ReceiveData(USART1);
+        if(sign == 'W')
+        {
+            w_received = 1;
+        }
+
+        USART_SendData(USART1, '!');
+    }
+}
+
+void RCC_conf(void)
+{
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+    SysTick_Config(SystemCoreClock / 5); //SystemCoreClock - ile na sekunde; arg - do ilu musi doliczy
+}
+
+void GPIO_conf(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_1);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_1);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2
+            | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+void USART_conf(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+
+    USART_InitStructure.USART_BaudRate = 9600;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+    USART_Init(USART1, &USART_InitStructure);
+    USART_Cmd(USART1, ENABLE);
+}
+
+void NVIC_conf(void)
+{
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
 }
