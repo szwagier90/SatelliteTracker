023139faf34d9c6a5474da7e62512034902fbcff
szwagier90 <szwagier90@gmail.com>
2013-11-16 22:19:09 +0100

    DDE Reading
    Done ugly version of reading Azimuth and Elevation coordinate from DDE
    protocol.
    Azimuth is sent to GPIOA

--- Project/interrupts.c
+++ Project/interrupts.c
@@ -6,31 +6,142 @@
 
 void SysTick_Handler(void)
 {
-    if(w_received)
-    {
-        sign = 'W';
-        GPIO_Write(GPIOA, sign);
-    }
-    else
-    {
-        sign += 1;
-        GPIO_Write(GPIOA, sign);
-
-    }
-
-    USART_SendData(USART1, sign);
+    GPIO_Write(GPIOA, azimuth);
 }
 
 void USART1_IRQHandler(void)
 {
-    if(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == SET)
+    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
     {
-        sign = USART_ReceiveData(USART1);
-        if(sign == 'W')
-        {
-            w_received = 1;
-        }
+        received = USART_ReceiveData(USART1);
 
-        USART_SendData(USART1, '!');
+        if(WORD_WAIT_FOR_DATA == dde_word_state)
+            word_wait_for_data();
+        else if(WORD_BEGIN == dde_word_state)
+            word_begin();
+        else if(WORD_AZIMUTH == dde_word_state)
+            word_azimuth();
+        else if(WORD_AZIMUTH_CHECKED == dde_word_state)
+            word_azimuth_checked();
+        else if(WORD_ELEVATION == dde_word_state)
+            word_elevation();
+        else if(WORD_COORDINATES_STORED == dde_word_state)
+            word_coordinates_stored();
+
+        USART_SendData(USART1, received);
     }
+}
+
+inline void word_wait_for_data()
+{
+    if(ASCII_DDE_STRING_BEGIN == received)
+        dde_word_state = WORD_BEGIN;
+}
+
+inline void word_begin()
+{
+    if(is_digit(received))
+    {
+        azimuth_buffer = atoi(received);
+        dde_word_state = WORD_AZIMUTH;
+    }
+    else if(received != 'W')
+        dde_word_state = WORD_WAIT_FOR_DATA;
+}
+
+inline void word_azimuth()
+{
+    if(is_digit(received))
+    {
+        azimuth_buffer *= 10;
+        azimuth_buffer += atoi(received);
+    }
+    else if(ASCII_DDE_STRING_BEGIN == received)
+        dde_word_state = WORD_BEGIN;
+    else if(' ' == received)
+    {
+        if(is_azimuth_ok())
+            dde_word_state = WORD_AZIMUTH_CHECKED;
+        else
+            dde_word_state = WORD_WAIT_FOR_DATA;
+    }
+    else
+    {
+        dde_word_state = WORD_WAIT_FOR_DATA;
+    }
+}
+
+inline void word_azimuth_checked()
+{
+    if(is_digit(received))
+    {
+        elevation_buffer = atoi(received);
+        dde_word_state = WORD_ELEVATION;
+    }
+    else if(ASCII_DDE_STRING_BEGIN == received)
+        dde_word_state = WORD_BEGIN;
+    else
+        dde_word_state = WORD_WAIT_FOR_DATA;
+}
+
+inline void word_elevation()
+{
+    if(is_digit(received))
+    {
+        elevation_buffer *= 10;
+        elevation_buffer += atoi(received);
+    }
+    else if(ASCII_DDE_STRING_BEGIN == received)
+        dde_word_state = WORD_BEGIN;
+    else if(ASCII_DDE_STRING_END == received)
+    {
+        if(is_elevation_ok())
+        {
+            azimuth = azimuth_buffer;
+            elevation = elevation_buffer;
+            dde_word_state = WORD_COORDINATES_STORED;
+        }
+        else
+            dde_word_state = WORD_WAIT_FOR_DATA;
+
+    }
+    else
+        dde_word_state = WORD_WAIT_FOR_DATA;
+}
+
+inline void word_coordinates_stored()
+{
+    if(ASCII_DDE_STRING_BEGIN == received)
+        dde_word_state = WORD_BEGIN;
+    else
+        dde_word_state = WORD_WAIT_FOR_DATA;
+}
+
+inline uint8_t is_digit(uint8_t c)
+{
+    if((c >= '0') && (c <= '9'))
+        return (uint8_t)1;
+    else
+        return (uint8_t)0;
+}
+
+inline uint8_t is_azimuth_ok()
+{
+    if((azimuth_buffer >= 0) && (azimuth_buffer <= 360))
+        return 1;
+    else
+        return 0;
+}
+
+inline uint8_t is_elevation_ok()
+{
+    if((elevation_buffer >= 0) && (elevation_buffer <= 360))
+        return 1;
+    else
+        return 0;
+}
+
+inline uint8_t atoi(uint8_t a)
+{
+    return a - ASCII_ZERO;
 }
--- Project/interrupts.h
+++ Project/interrupts.h
@@ -3,10 +3,42 @@
 
 #include "stm32f0xx_rcc.h"
 
-volatile uint8_t sign = '0';
-volatile uint8_t w_received = 0;
+const uint8_t ASCII_ZERO = '0';
+const uint8_t ASCII_DDE_STRING_BEGIN = 'W';
+const uint8_t ASCII_DDE_STRING_END = '\r';
+
+volatile enum
+{
+    WORD_WAIT_FOR_DATA,
+    WORD_BEGIN = 0x01,
+    WORD_AZIMUTH = 0x02,
+    WORD_AZIMUTH_CHECKED = 0x04,
+    WORD_ELEVATION = 0x08,
+    WORD_COORDINATES_STORED = 0x10
+} dde_word_state = WORD_WAIT_FOR_DATA;
+
+volatile uint8_t received = 0x0;
+
+volatile uint8_t azimuth = 0x0;
+volatile uint8_t elevation = 0x0;
+volatile uint8_t azimuth_buffer = 0x0;
+volatile uint8_t elevation_buffer = 0x0;
+
 
 void SysTick_Handler(void);
 void USART1_IRQHandler(void);
+
+inline void word_wait_for_data();
+inline void word_begin();
+inline void word_azimuth();
+inline void word_azimuth_checked();
+inline void word_elevation();
+inline void word_coordinates_stored();
+
+inline uint8_t is_digit(uint8_t);
+inline uint8_t is_azimuth_ok();
+inline uint8_t is_elevation_ok();
+inline uint8_t atoi(uint8_t);
+
 
 #endif
